e721ef0deac7489bf8e4589c5464cca8db39bed3
Om Londhe <oplondhe@gmail.com>
2022-04-09 19:48:14 +0530

    After completing auth functionality

--- server.py
+++ server.py
@@ -1,19 +1,56 @@
+import json
 from flask_pymongo import PyMongo
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
     return '<h1>Hello World</h1>'
 
-@app.route('/create')
-def create_collection():
-    try:
-        print(mongo.db.create_collection("test"))
-    except Exception as e:
-        print(e)
-    return 'done'
+
+@app.route('/check-username', methods=["GET"])
+def check_username():
+    if request.method == "GET":
+        username = request.args.get("username")
+        result = mongo.db.users.find({"username": username})
+        for _ in result:
+            return json.dumps({"exist": True})
+        return json.dumps({"exist": False})
+
+
+@app.route('/check-email', methods=["GET"])
+def check_email():
+    if request.method == "GET":
+        email = request.args.get("email")
+        result = mongo.db.users.find({"email": email})
+        for _ in result:
+            return json.dumps({"exist": True})
+        return json.dumps({"exist": False})
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    if request.method == "POST":
+        name = request.form["name"] 
+        username = request.form["username"] 
+        email = request.form["email"]
+        print(name)
+        print(username)
+        print(email)
+        mongo.db.users.insert_one({
+            "name": name, 
+            "username": username, 
+            "email": email, 
+            "bio": "", 
+            "profileImage": "", 
+            "coverImage": ""
+        })
+        return {"registered": True}
+    return {"registered": False}
+
 
 if __name__ == '__main__':
     app.config["DEBUG"] = True
